eaf1ca15d01b3a88a417ba7551915977e765dcd9
tulip31518 <tulip31518@outlook.com>
2018-08-25 10:13:56 -0700

    fade

--- assets/scripts/menu_mode.js
+++ assets/scripts/menu_mode.js
@@ -33,13 +33,21 @@ cc.Class({
 
     onLoad () {
 
+        // this.canvas.runAction(cc.sequence(
+        //     cc.tintTo(2, 255, 0, 0),
+        //     cc.delayTime(0.5),
+        //     cc.fadeTo(1, 50),
+        //     cc.delayTime(0.5),
+        //     cc.fadeTo(1, 255),
+        //     cc.delayTime(0.5),
+        //     cc.tintTo(2, 255, 255, 255)
+        // ));
         this.game = this.canvas.getComponent('Game');
         this.node.on(cc.Node.EventType.MOUSE_DOWN, function () {
+            // this.set_font(this.game.game_level);
             this.in_panel();
         }, this);
-
-        this.normal.on(cc.Node.EventType.MOUSE_DOWN, function ()
-        {            
+        this.normal.on(cc.Node.EventType.MOUSE_DOWN, function (){            
             this.load_level(0);
         }, this);
         this.hard.on(cc.Node.EventType.MOUSE_DOWN, function () {            
@@ -51,20 +59,46 @@ cc.Class({
         this.extreme.on(cc.Node.EventType.MOUSE_DOWN, function () {            
             this.load_level(3);
         }, this);
+
+        this.lobbyAppearAction1 = cc.moveBy(0.4, cc.v2(0, 480)).easing(cc.easeElasticOut());
+        this.lobbyAppearAction2 = cc.moveBy(0.5, cc.v2(0, 480)).easing(cc.easeElasticOut());
+        this.lobbyAppearAction3 = cc.moveBy(0.6, cc.v2(0, 480)).easing(cc.easeElasticOut());
+        this.lobbyAppearAction4 = cc.moveBy(0.7, cc.v2(0, 480)).easing(cc.easeElasticOut());
+
+        this.lobbyDisappearAction1 = cc.moveBy(0.7, cc.v2(0, -480)).easing(cc.easeElasticOut());
+        this.lobbyDisappearAction2 = cc.moveBy(0.6, cc.v2(0, -480)).easing(cc.easeElasticOut());
+        this.lobbyDisappearAction3 = cc.moveBy(0.5, cc.v2(0, -480)).easing(cc.easeElasticOut());
+        this.lobbyDisappearAction4 = cc.moveBy(0.4, cc.v2(0, -480)).easing(cc.easeElasticOut());
+        
+        this.fadeToDark = cc.fadeOut(0.5);
+        this.fadeToLight = cc.fadeIn(0.5);
     },
 
     in_panel: function () {
-        var movement = cc.moveTo(0.3, cc.v2(0, 960));
-        this.setting_panel.runAction(movement);
-        // this.canvas.color.a = 50;
+        
+        this.canvas.runAction(cc.sequence(
+            cc.fadeTo(1, 50),
+            cc.delayTime(0.1)
+        ));
+        this.normal.runAction(cc.sequence(cc.moveBy(0.4, cc.v2(0, 480)), this.lobbyAppearAction1));
+        this.hard.runAction(cc.sequence(cc.moveBy(0.5, cc.v2(0, 480)), this.lobbyAppearAction2));
+        this.hell.runAction(cc.sequence(cc.moveBy(0.6, cc.v2(0, 480)), this.lobbyAppearAction3));
+        this.extreme.runAction(cc.sequence(cc.moveBy(0.7, cc.v2(0, 480)), this.lobbyAppearAction4));
+        
         this.setting_panel.zIndex = 999;
     },
 
     out_panel: function () {
-        // this.canvas.color.a = 255;
-        var movement = cc.moveTo(0.3, cc.v2(0, 0));
-        this.setting_panel.runAction(movement);        
-        this.setting_panel.zIndex = 0;        
+        this.canvas.runAction(cc.sequence(            
+            cc.delayTime(0.1),
+            cc.fadeTo(1, 255),
+            cc.delayTime(0.1)
+        ));       
+        this.normal.runAction(cc.sequence(cc.moveBy(0.7, cc.v2(0, -480)), this.lobbyDisappearAction1));
+        this.hard.runAction(cc.sequence(cc.moveBy(0.6, cc.v2(0, -480)), this.lobbyDisappearAction2));
+        this.hell.runAction(cc.sequence(cc.moveBy(0.5, cc.v2(0, -480)), this.lobbyDisappearAction3));
+        this.extreme.runAction(cc.sequence(cc.moveBy(0.4, cc.v2(0, -480)), this.lobbyDisappearAction4));        
+        // this.setting_panel.zIndex = 0;                
     },
 
    load_level: function (lvl) {
@@ -75,6 +109,22 @@ cc.Class({
             this.game.create_table();
         }   
         this.out_panel();        
+    },
+
+    set_font: function (lvl) {
+
+        // this.normal.font.size = 40;
+        // this.hard.font.size = 40;
+        // this.hell.font.size = 40;
+        // this.extreme.font.size = 40;
+        // switch(lvl)
+        // {
+        //     case 0:this.normal.font.size = 80;break;
+        //     case 1:this.hard.font.size = 80;break;
+        //     case 2:this.hell.font.size = 80;break;
+        //     default:this.extreme.font.size = 80;break;
+        // } 
+                
     },
 
     // update (dt) {},
